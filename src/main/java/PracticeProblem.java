5271d090cc2a1b643278379379af71387b30d5df
BensonZhengFromACCI <343804126@tdsb.ca>
2026-02-11 19:16:47 +0000

    hey gang

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,24 +12,35 @@ public class PracticeProblem {
 		int num1 = 2;
 		int num2 = 5;
 		int num3 = 3;
-		int out = (num1+num2+num3);
-		System.out.println(out);
+		System.out.println(num1 + num2 + num3);
+		System.out.println(num1 * num2 * num3);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int div1 = 10;
+		int div2 = 3;
+		System.out.println(div1 / div2); 
+		System.out.println(div1 % div2); 
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		double dec1 = 5.3;
+		double dec2 = 5.2;
+		System.out.println(dec1 / dec2);
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		int num1 = 2;
+		int num2 = 3;
+		int num3 = 4;
+		int numOut = (num1+num2);
+		System.out.println(numOut *num3);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		int num1 = 5;
+		double num2 = (num1 + 3.5);
+		System.out.println(num2);
 	}
 
 }
