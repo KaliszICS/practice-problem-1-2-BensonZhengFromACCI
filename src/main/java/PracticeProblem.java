5da2d13baf5016704cb92201be5d904a63da659a
BensonZhengFromACCI <343804126@tdsb.ca>
2026-02-11 16:25:39 +0000

    your

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -9,7 +9,11 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		int num1 = 2;
+		int num2 = 5;
+		int num3 = 3;
+		int out = (num1+num2+num3);
+		System.out.println(out);
 	}
 
 	public static void q2() {
